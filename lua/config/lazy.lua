01120eeeb32a32f4b5b50393ab3094b608538846
Patrick O'Shea <p14oshea@gmail.com>
2024-06-17 18:31:50 +0100

    Remove edgy and animate

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,8 +23,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.editor.illuminate" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
-    { import = "lazyvim.plugins.extras.ui.edgy" },
-    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
