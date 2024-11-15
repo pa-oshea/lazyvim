afddea5d25f498bb8f7850e0b068ea3bfe767dbf
Patrick O'Shea <p14oshea@gmail.com>
2024-11-15 11:27:05 +0000

    neotest

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,106 +1,14 @@
 return {
   {
-    "rcasia/neotest-java",
-    tag = "v0.10.0",
-    ft = "java",
-    dependencies = {
-      "nvim-neotest/neotest",
-      dependencies = {
-        "nvim-neotest/nvim-nio",
-        "nvim-lua/plenary.nvim",
-        "antoinemadec/FixCursorHold.nvim",
-        "nvim-treesitter/nvim-treesitter",
-      },
-    },
-    keys = {
-      { "<leader>tn", "", desc = "+neotest" },
-      {
-        "<leader>tnt",
-        function()
-          require("neotest").run.run(vim.fn.expand("%"))
-        end,
-        desc = "Run File",
-      },
-      {
-        "<leader>tnT",
-        function()
-          require("neotest").run.run(vim.uv.cwd())
-        end,
-        desc = "Run All Test Files",
-      },
-      {
-        "<leader>tnr",
-        function()
-          require("neotest").run.run()
-        end,
-        desc = "Run Nearest",
-      },
-      {
-        "<leader>tnl",
-        function()
-          require("neotest").run.run_last()
-        end,
-        desc = "Run Last",
-      },
-      {
-        "<leader>tns",
-        function()
-          require("neotest").summary.toggle()
-        end,
-        desc = "Toggle Summary",
-      },
-      {
-        "<leader>tno",
-        function()
-          require("neotest").output.open({ enter = true, auto_close = true })
-        end,
-        desc = "Show Output",
-      },
-      {
-        "<leader>tnO",
-        function()
-          require("neotest").output_panel.toggle()
-        end,
-        desc = "Toggle Output Panel",
-      },
-      {
-        "<leader>tnS",
-        function()
-          require("neotest").run.stop()
-        end,
-        desc = "Stop",
-      },
-      {
-        "<leader>tnw",
-        function()
-          require("neotest").watch.toggle(vim.fn.expand("%"))
-        end,
-        desc = "Toggle Watch",
-      },
-    },
-    config = function()
-      require("neotest").setup({
-        adapters = {
-          require("neotest-java")({
-            ignore_wrapper = false, -- whether to ignore maven/gradle wrapper
-          }),
-        },
-        output = { open_on_run = true },
-        quickfix = {
-          open = function()
-            vim.cmd("copen")
-          end,
-        },
-      })
-    end,
-  },
-  {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "fredrikaverpil/neotest-golang",
+      "rcasia/neotest-java",
+      "nvim-neotest/neotest-jest",
     },
     opts = {
       adapaters = {
