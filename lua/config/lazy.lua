10f48568077bd77b0858fe4694aa155c344e1f5d
Patrick O'Shea <p14oshea@gmail.com>
2024-11-15 11:26:13 +0000

    Add rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.harpoon2" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
