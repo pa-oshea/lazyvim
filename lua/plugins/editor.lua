45605ee35575c3aa0b7f48749cfd4a841c0a26cc
Patrick O'Shea <p14oshea@gmail.com>
2025-01-12 17:32:56 +0000

    quicker plugin

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -37,4 +37,12 @@ return {
       },
     },
   },
+  {
+    "stevearc/quicker.nvim",
+    event = "FileType qf",
+    ---@module "quicker"
+    ---@type quicker.SetupOptions
+    opts = {},
+    config = true,
+  },
 }
